f2f588a895e477a5738fe356f72cf8e570d0ee22
Realharshagarwal <Realharshagarwal@gmail.com>
2025-10-25 18:59:02 +0530

    Setting up Backend Structure of the Project

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`server is running at ${PORT}`);
+})
